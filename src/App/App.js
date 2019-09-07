156d3ea958d0f2721d4884aa0ee46f03b9c72bb5
Brandy Mello <brandyamello@gmail.com>
2019-09-07 12:03:57 -0600

    Fixed filepath for form

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import LoginForm from '../components/Forms/LoginForm/LoginForm';
+import LoginForm from '../containers/LoginForm/LoginForm';
 import { landingFetch, authorFetch } from '../util/apiCalls';
 import './App.css';
 import NewUserForm from '../containers/NewUserForm';
