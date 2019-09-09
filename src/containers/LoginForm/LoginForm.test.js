bf43e44df583dadbb655c9b810eacb5963e95a57
Brianna DelValle <briannadelvalle@gmail.com>
2019-09-09 14:17:18 -0600

    Set up dummy test in each component test file.

---
+++ src/containers/LoginForm/LoginForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoginForm from './LoginForm';
+
+describe('LoginForm', () => {
+  it('should match the snapshot with correct data passed in', () => {
+    expect(true).toEqual(true)
+  })
+})
---
+++ src/containers/NewUserForm/NewUserForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewUserForm from './NewUserForm';
+
+describe('NewUserForm', () => {
+  it('should match the snapshot with correct data passed in', () => {
+    expect(true).toEqual(true)
+  })
+})
