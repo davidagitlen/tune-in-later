58778b61ad6016a68013ad8f0cfb2362a0633100
Brandy Mello <brandyamello@gmail.com>
2019-09-09 16:23:38 -0600

    Move mock variables outside nested describe block to allow access to other describe blocks within the loginForm testing file

--- src/containers/LoginForm/LoginForm.test.js
+++ src/containers/LoginForm/LoginForm.test.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { LoginForm, mapStateToProps, mapDispatchToProps } from './LoginForm';
 import { shallow } from 'enzyme';
 
-describe('LoginForm', () => {
+describe('LoginFormContainer', () => {
   let wrapper;
 
   const mockSetCurrentUser = jest.fn();
   const mockSetCurrentUserFavorites = jest.fn();
+  const mockCurrentUser = {
+    id: 1,
+    name: "Joan",
+    email: "exceeding@this.io"
+  };
+
   const mockFavorites = [{
     id: 11,
     book_id: 918578041,
@@ -18,25 +24,25 @@ describe('LoginForm', () => {
     description: "Tim Tebow is one of the most talked about athletes of all time. Everybody seems to have an opinion about him, especially on his religious beliefs. But, in order to truly understand Tim Tebow, you must first understand where he came from. <i>Tim Tebow: An Unauthorized Biography</i> explores Tebow's upbringing, his high school and college football careers, and details his breakthrough into the NFL, all the way through his playoff loss to the New England Patriots. <br /><br />''Tebow-mania'' has fans and critics alike questioning his motives and looking for where God and his career will take him next. Inside, you will discover where it all started and learn why so many fans are ''Tebowing'' alongside their new national hero. From family to football to the future, all you need to know about Tim Tebow is right here, right now, in the most up to date coverage of Tim Tebow's life. <br /><br />This audiobook also includes all of Tim Tebow's college and professional football statistics.",
     primary_genre_name: "Biographies & Memoirs"
   }];
-  const mockCurrentUser = {
-    id: 1,
-    name: "Joan",
-    email: "exceeding@this.io"
-  };
-  
-  beforeEach(() => {
-    wrapper = shallow(<LoginForm 
-      currentUser={ mockCurrentUser } 
-      favorites={ mockFavorites } 
-      setCurrentUser={ mockSetCurrentUser } 
-      setCurrentUserFavorites={ mockSetCurrentUserFavorites } 
-    />)
 
+  beforeEach(() => {
+    wrapper = shallow(<LoginForm
+      currentUser={mockCurrentUser}
+      favorites={mockFavorites}
+      setCurrentUser={mockSetCurrentUser}
+      setCurrentUserFavorites={mockSetCurrentUserFavorites}
+    />)
   });
 
-    it('should match the snapshot when a user is logged in', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
+  describe('LoginForm', () => {
+    
+      it('should match the snapshot when a user is logged in', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
 
-    // it('should return a current user')
+  // describe('mapStateToProps', () => {
+  //     it('should return a current user', () => {
+
+  //     });
+  });
 });
