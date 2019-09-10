92cea675a47d3840d059205e971d974ae67eabf9
Brianna DelValle <briannadelvalle@gmail.com>
2019-09-10 14:26:00 -0600

    Write snapshot test for SearchForm

--- src/containers/SearchForm/SearchForm.test.js
+++ src/containers/SearchForm/SearchForm.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SearchForm from './SearchForm';
+import { SearchForm } from './SearchForm';
+
+
+
 
 describe('SearchForm', () => {
+
+  let wrapper; 
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <SearchForm 
+        displaySearchResults={jest.fn()}
+      />
+    )
+  })
+
   it('should match the snapshot with correct data passed in', () => {
-    expect(true).toEqual(true)
+    expect(wrapper).toMatchSnapshot();
   })
 })
