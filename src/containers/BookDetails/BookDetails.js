37dff480c0b63b37c1e25e94b10599f77c3c8650
davidagitlen <davidagitlen@gmail.com>
2019-09-10 10:38:00 -0600

    Remove price display from book details, saved collection cannot contain price details because of back end api formatting

--- src/containers/BookDetails/BookDetails.js
+++ src/containers/BookDetails/BookDetails.js
@@ -13,7 +13,6 @@ const BookDetails = ({book}) => {
         <h2>{title}</h2>
         <h3>{author}</h3>
         <p>{genre} </p>
-        <p>${price}</p>
       </div>
       <div className="description">
       <p>{description}</p>
