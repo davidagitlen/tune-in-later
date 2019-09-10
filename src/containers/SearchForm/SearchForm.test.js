44fefdbb4b61e5bfd5a433fde14edcdb021a763d
Brianna DelValle <briannadelvalle@gmail.com>
2019-09-10 14:33:06 -0600

    Setup describe blocks for SearchForm tests

--- src/containers/SearchForm/SearchForm.test.js
+++ src/containers/SearchForm/SearchForm.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SearchForm from './SearchForm';
+import { SearchForm } from './SearchForm';
+import { fetchSearch } from '../../util/apiCalls';
+
+jest.mock('../../util/apiCalls');
 
 describe('SearchForm', () => {
-  it('should match the snapshot with correct data passed in', () => {
-    expect(true).toEqual(true)
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+    <SearchForm 
+      displaySearchResults={jest.fn()}
+    />)
   })
+  it('should match the snapshot with correct data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  describe('handleSearchInput', () => {
+
+  })
+
+  describe('clearSearch', () => {
+
+  })
+
+  describe('handleSearch', () => {
+
+  })
+
+  describe('cleanSearchResults', () => {
+
+  })
+
+  describe('matchDispatchToProps', () => {
+    
+  })
+
 })
