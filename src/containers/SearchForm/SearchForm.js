f13bb4d77daee7f2e9845bd65986ab4f968dde3a
davidagitlen <davidagitlen@gmail.com>
2019-09-10 11:12:56 -0600

    Update search results display to render each book with a unique key based on collectionId

--- src/containers/SearchForm/SearchForm.js
+++ src/containers/SearchForm/SearchForm.js
@@ -39,8 +39,10 @@ class SearchForm extends Component {
 
   cleanSearchResults = (rawSearchResults) => {
     const allBooks = rawSearchResults.map(datum => datum).flat();
-    let regex = new RegExp('(&nbsp;|<([^>]+)>)', 'g')
+    let regex = new RegExp('(&nbsp;|<([^>]+)>)', 'g');
+    console.log(allBooks)
     const formattedBooks = allBooks.map(book => ({
+      id: book.collectionId,
       artist: book.artistName,
       image: book.artworkUrl100,
       price: book.collectionPrice,
